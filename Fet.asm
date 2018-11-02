1b833780a456c4facd84c3a48a68f1f3ebad04b1
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-02 17:12:38 +0100

    Include win32ax.inc

--- Fet.asm
+++ Fet.asm
@@ -1,6 +1,8 @@
 format PE GUI 6.1
 entry main
 
+include 'INCLUDE/win32ax.inc'
+
 section '.text' code executable
 
 main:
@@ -8,3 +10,6 @@ main:
 section '.idata' data readable import
 
 library kernel32, 'kernel32.dll'
+
+import kernel32, \
+       OpenProcess, 'OpenProcess'
