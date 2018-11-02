a1f6a48f516541ebdc04b443e232a48127ad0d65
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-02 17:50:15 +0100

    Update and fix errors

--- Fet.asm
+++ Fet.asm
@@ -3,18 +3,34 @@ entry main
 
 include 'INCLUDE/win32ax.inc'
 
+struct PROCESSENTRY32
+        dwSize                  dd ?
+        cntUsage                dd ?
+        th32ProcessID           dd ?
+        th32DefaultHeapID       dd ?
+        th32ModuleID            dd ?
+        cntThreads              dd ?
+        th32ParentProcessID     dd ?
+        pcPriClassBase          dd ?
+        dwFlags                 dd ?
+        szExeFile               dw MAX_PATH dup (?)
+ends
+
 section '.text' code executable
 
 main:
+    stdcall findProcessId
     invoke ExitProcess, 1
 
 proc findProcessId
     locals
         processEntry PROCESSENTRY32 ?
-        snapshot HANDLE ?
+        snapshot dd ?
     endl
 
-    mov [processEntry.dwSize], sizeof(PROCESSENTRY32)
+    mov [processEntry.dwSize], sizeof.PROCESSENTRY32
+    invoke CreateToolhelp32Snapshot, 0x2, 0
+    ret
 endp
 
 section '.idata' data readable import
