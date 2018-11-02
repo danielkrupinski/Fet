b68379ed84a009bd74eb4a4757c1bc2ee8398129
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-02 18:52:40 +0100

    Import Module32First and Module32Next from kernel32.dll

--- Fet.asm
+++ Fet.asm
@@ -68,7 +68,32 @@ proc findProcessId
 endp
 
 proc findModuleBase
+    locals
+        moduleEntry MODULEENTRY32 ?
+        snapshot dd ?
+    endl
 
+    invoke CreateToolhelp32Snapshot, 0x8, pid
+    mov [snapshot], eax
+    mov [moduleEntry.dwSize], sizeof.MODULEENTRY32
+    lea eax, [snapshot]
+    lea ebx, [moduleEntry]
+    invoke Module32First, dword [eax], ebx
+    cmp eax, 1
+    jne error
+    loop1:
+        lea eax, [snapshot]
+        lea ebx, [processEntry]
+        invoke Module32Next, dword [eax], ebx
+        cmp eax, 1
+        jne error
+        lea eax, [processEntry.szExeFile]
+        cinvoke strcmp, <'csgo.exe', 0>, eax
+        cmp eax, 0
+        jne loop1
+
+    mov eax, [processEntry.th32ProcessID]
+    ret
 endp
 
 section '.idata' data readable import
@@ -78,6 +103,8 @@ library kernel32, 'kernel32.dll', \
 
 import kernel32, \
        CreateToolhelp32Snapshot, 'CreateToolhelp32Snapshot', \
+       Module32First, 'Module32First', \
+       Module32Next, 'Module32Next', \
        OpenProcess, 'OpenProcess', \
        Process32First, 'Process32First', \
        Process32Next, 'Process32Next', \
