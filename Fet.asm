4239b73e569fb244e242a4df98c1e9cdaf205502
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-18 11:07:55 +0100

    Remove unncessary Sleep call

--- Fet.asm
+++ Fet.asm
@@ -44,6 +44,7 @@ main:
     cinvoke printf, <'Handle: %d', 0>, eax
 
     triggerbot:
+	invoke Sleep, 1
     lea eax, [localPlayer]
     mov ebx, [clientBase]
     add ebx, [localPlayerOffset]
@@ -84,7 +85,6 @@ main:
     add eax, [forceAttackOffset]
     lea ebx, [force1]
     invoke WriteProcessMemory, dword [processHandle], eax, ebx, 4, NULL
-    invoke Sleep, 1
     mov eax, [clientBase]
     add eax, [forceAttackOffset]
     lea ebx, [force2]
