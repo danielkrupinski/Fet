3164dd84f87c7a570caa0a785971c8e2eab5ce4b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-25 19:56:06 +0100

    Update findProcessId and findModuleBase procedures

--- Fet.asm
+++ Fet.asm
@@ -120,15 +120,13 @@ proc findProcessId
     invoke CreateToolhelp32Snapshot, 0x2, 0
     mov [snapshot], eax
     mov [processEntry.dwSize], sizeof.PROCESSENTRY32
-    lea eax, [snapshot]
-    lea ebx, [processEntry]
-    invoke Process32First, dword [eax], ebx
+    lea eax, [processEntry]
+    invoke Process32First, [snapshot], eax
     cmp eax, 1
     jne exit
     loop2:
-        lea eax, [snapshot]
-        lea ebx, [processEntry]
-        invoke Process32Next, dword [eax], ebx
+        lea eax, [processEntry]
+        invoke Process32Next, [snapshot], eax
         cmp eax, 1
         jne exit
         lea eax, [processEntry.szExeFile]
@@ -149,15 +147,13 @@ proc findModuleBase, processID
     invoke CreateToolhelp32Snapshot, 0x8, [processID]
     mov [snapshot], eax
     mov [moduleEntry.dwSize], sizeof.MODULEENTRY32
-    lea eax, [snapshot]
-    lea ebx, [moduleEntry]
-    invoke Module32First, dword [eax], ebx
+    lea eax, [moduleEntry]
+    invoke Module32First, [snapshot], eax
     cmp eax, 1
     jne exit
     loop3:
-        lea eax, [snapshot]
-        lea ebx, [moduleEntry]
-        invoke Module32Next, dword [eax], ebx
+        lea eax, [moduleEntry]
+        invoke Module32Next, [snapshot], eax
         cmp eax, 1
         jne exit
         lea eax, [moduleEntry.szModule]
